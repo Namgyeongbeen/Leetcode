2e52345d487c903b80857003fb890188af0a0e73
Nam gyeongbeen <152850843+Namgyeongbeen@users.noreply.github.com>
2023-12-11 16:10:02 +0900

    Time: 1815 ms (5.02%) | Memory: 0B (100.00%) - LeetSync

--- 1179-game-play-analysis-i/game-play-analysis-i.sql
+++ 1179-game-play-analysis-i/game-play-analysis-i.sql
@@ -1,5 +1,19 @@
 # Write your MySQL query statement below
-SELECT player_id
-     , min(date(event_date)) first_login
- FROM ACTIVITY
- GROUP BY player_id;
+
+# window함수 안 쓰고 풀기
+# SELECT player_id
+#      , min(date(event_date)) first_login
+#  FROM ACTIVITY
+#  GROUP BY player_id;
+
+# window함수 쓰고 풀기
+select player_id
+     , case when 1st = 1 then event_date end first_login
+  from (
+  select player_id
+      , row_number () over(partition by player_id order by event_date) AS 1st
+      , event_date
+    from activity
+    order by player_id
+  ) login_date
+  where case when 1st = 1 then 1st end is not null;
