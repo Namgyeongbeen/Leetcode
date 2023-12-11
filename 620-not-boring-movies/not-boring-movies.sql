32ff9bea99fd72dd74505aa6d640691c93c4ea45
Nam gyeongbeen <152850843+Namgyeongbeen@users.noreply.github.com>
2023-12-11 12:47:52 +0900

    Time: 430 ms (39.86%) | Memory: 0B (100.00%) - LeetSync

--- 620-not-boring-movies/not-boring-movies.sql
+++ 620-not-boring-movies/not-boring-movies.sql
@@ -1,14 +1,6 @@
 # Write your MySQL query statement below
-WITH odd_id AS(
-SELECT CASE WHEN id%2 != 0 THEN id END AS id
-     , movie
-     , description
-     , rating
+SELECT *
   FROM Cinema
-)
-
-select *
-  FROM odd_id
-  WHERE id IS NOT NULL
+  WHERE id%2 != 0
     AND description != 'boring'
   ORDER BY rating DESC;
