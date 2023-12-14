1e7a08fdd5bf72370148147388180c943c79635c
Nam gyeongbeen <152850843+Namgyeongbeen@users.noreply.github.com>
2023-12-14 17:14:10 +0900

    Time: 1047 ms (51.74%) | Memory: 0B (100.00%) - LeetSync

--- 262-trips-and-users/trips-and-users.sql
+++ 262-trips-and-users/trips-and-users.sql
@@ -14,14 +14,27 @@
 #   GROUP BY t.request_at;
 
 # 솔루션 코드 중 하나. 내것과 가장 유사하다.
-SELECT request_at as Day, 
-       ROUND(COUNT(CASE 
-             WHEN status != 'completed' 
-             THEN 1 
-             ELSE NULL 
-             END) / COUNT(*),2) AS "Cancellation Rate"
-FROM Trips
-INNER JOIN Users client ON client.users_id = Trips.client_id
-INNER JOIN Users driver ON driver.users_id = Trips.driver_id
-WHERE client.banned = 'No' AND driver.banned = 'No' AND request_at >= "2013-10-01" AND request_at <= "2013-10-03"
-GROUP BY request_at
+# SELECT request_at as Day, 
+#        ROUND(COUNT(CASE 
+#              WHEN status != 'completed' 
+#              THEN 1 
+#              ELSE NULL 
+#              END) / COUNT(*),2) AS "Cancellation Rate"
+# FROM Trips
+# INNER JOIN Users client ON client.users_id = Trips.client_id
+# INNER JOIN Users driver ON driver.users_id = Trips.driver_id
+# WHERE client.banned = 'No' AND driver.banned = 'No' AND request_at >= "2013-10-01" AND request_at <= "2013-10-03"
+# GROUP BY request_at
+
+
+select t.request_at as Day
+     , round(sum(case when t.status LIKE '%cancel%' then 1 else 0 end)/count(t.request_at), 2) as "Cancellation Rate"
+  from Trips t
+  inner join users c
+    on t.client_id = c.users_id
+  inner join users d
+    on t.driver_id = d.users_id
+  where t.request_at between '2013-10-01' and '2013-10-03'
+    and c.banned = 'No'
+    and d.banned = 'No'
+  group by t.request_at;
