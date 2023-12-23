9b59fbebb44a48de3c6f9ec31504d2bd79050305
Gyeongbeen <152850843+Namgyeongbeen@users.noreply.github.com>
2023-12-23 15:43:00 +0900

    Time: 2288 ms (43.33%) | Memory: 0B (100.00%) - LeetSync

--- 1509-replace-employee-id-with-the-unique-identifier/replace-employee-id-with-the-unique-identifier.sql
+++ 1509-replace-employee-id-with-the-unique-identifier/replace-employee-id-with-the-unique-identifier.sql
@@ -1,7 +1,7 @@
 # Write your MySQL query statement below
 # 각 유저별 unique id를 출력하고, unique id가 없는 유저에게는 null을 출력.
-select unique_id
-     , name
+select e2.unique_id
+     , e1.name
   from Employees e1
   left join EmployeeUNI e2
     on e1.id = e2.id
