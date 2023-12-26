c75224f0a9ce7c129c249309a363ced21e6325f8
Gyeongbeen <152850843+Namgyeongbeen@users.noreply.github.com>
2023-12-26 14:14:30 +0900

    Time: 1092 ms (86.60%) | Memory: 0B (100.00%) - LeetSync

--- 184-department-highest-salary/department-highest-salary.sql
+++ 184-department-highest-salary/department-highest-salary.sql
@@ -1,8 +1,18 @@
 # Write your MySQL query statement below
-SELECT d.name Department, e.name Employee, e.salary Salary
-    FROM Employee e
-    inner JOIN Department d ON e.departmentId = d.id
-    WHERE (d.id, e.salary) IN
-        (SELECT departmentId, MAX(salary)
-        FROM Employee
-        GROUP BY departmentId);
+
+# 1. 부서별 max salary 출력하는 쿼리문을 서브쿼리로 만듦.
+# 2. employee 테이블에서 부서별 max salary의 쌍과 일치하는 데이터만 추출. 
+with max_salary as (
+  select departmentId
+       , max(salary) as salary
+    from Employee
+    group by departmentId
+)
+
+select d.name as Department
+     , e.name as Employee
+     , e.salary as Salary
+  from Employee e
+  left join Department d
+    on e.departmentId = d.id
+  where (e.departmentId, e.salary) in (select * from max_salary)
