5d9bf7e38f73da0e24fad80921c25d45d6927eae
Gyeongbeen <152850843+Namgyeongbeen@users.noreply.github.com>
2023-12-26 15:43:15 +0900

    Time: 907 ms (17.03%) | Memory: 0B (100.00%) - LeetSync

--- 178-rank-scores/rank-scores.sql
+++ 178-rank-scores/rank-scores.sql
@@ -6,10 +6,11 @@
 #   from Scores;
 
 # window 함수(X)
-SELECT S1.score
-     , COUNT(DISTINCT S2.score) AS 'rank'
-  FROM Scores S1
-  LEFT JOIN Scores S2
-    ON S2.score >= S1.score
-  GROUP BY S1.id
-  ORDER BY S1.score DESC;
+select s1.score
+     , count(distinct s2.score) as 'rank'
+  from Scores s1
+  left join Scores s2
+    on s1.score <= s2.score
+  group by s1.id
+  order by s1.score desc;
+  
