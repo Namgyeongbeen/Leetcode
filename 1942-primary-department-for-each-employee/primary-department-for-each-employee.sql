a254448bd4cb26c20444203e51a50f9d142526c8
Gyeongbeen <152850843+Namgyeongbeen@users.noreply.github.com>
2024-01-08 15:38:17 +0900

    Time: 1210 ms (23.52%) | Memory: 0B (100.00%) - LeetSync

--- 1942-primary-department-for-each-employee/primary-department-for-each-employee.sql
+++ 1942-primary-department-for-each-employee/primary-department-for-each-employee.sql
@@ -1,7 +1,7 @@
 # Write your MySQL query statement below
 SELECT employee_id,
        CASE WHEN COUNT(*) > 1 THEN MAX(CASE WHEN primary_flag = 'Y' THEN department_id END)
-            ELSE MAX(department_id) 
+            ELSE department_id 
        END AS department_id
   FROM Employee
   GROUP BY employee_id
