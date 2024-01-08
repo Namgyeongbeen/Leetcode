43a3af2e7791bee4777e37968538eadf6c332e56
Gyeongbeen <152850843+Namgyeongbeen@users.noreply.github.com>
2024-01-08 15:40:32 +0900

    Time: 1039 ms (50.84%) | Memory: 0B (100.00%) - LeetSync

--- 1942-primary-department-for-each-employee/primary-department-for-each-employee.sql
+++ 1942-primary-department-for-each-employee/primary-department-for-each-employee.sql
@@ -1,8 +1,15 @@
 # Write your MySQL query statement below
-SELECT employee_id,
-       CASE WHEN COUNT(*) > 1 THEN MAX(CASE WHEN primary_flag = 'Y' THEN department_id END)
-            ELSE department_id 
-       END AS department_id
-  FROM Employee
-  GROUP BY employee_id
+# SELECT employee_id,
+#        CASE WHEN COUNT(*) > 1 THEN MAX(CASE WHEN primary_flag = 'Y' THEN department_id END)
+#             ELSE department_id 
+#        END AS department_id
+#   FROM Employee
+#   GROUP BY employee_id
+#   having department_id is not null;
+
+select employee_id
+     , CASE WHEN COUNT(*) > 1 THEN MAX(CASE WHEN primary_flag = 'Y' THEN department_id END)
+            when count(department_id) = 1 then department_id end as department_id
+  from Employee
+  group by employee_id
   having department_id is not null;
