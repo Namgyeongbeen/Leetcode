9c16d0e7635088077e395763922c277cd1f32c3a
Gyeongbeen <152850843+Namgyeongbeen@users.noreply.github.com>
2024-01-15 11:17:53 +0900

    Time: 1562 ms (83.49%) | Memory: 0B (100.00%) - LeetSync

--- 1155-product-sales-analysis-iii/product-sales-analysis-iii.sql
+++ 1155-product-sales-analysis-iii/product-sales-analysis-iii.sql
@@ -1,19 +1,10 @@
 # Write your MySQL query statement below
-with base_table as(
-select product_id
-     , min(year)
-  from Sales
-  group by product_id
-)
-
 select product_id
      , year as first_year
      , quantity
      , price
-  from Sales
-  where (product_id, year) in (
-    select product_id
-         , min(year)
-      from Sales
-      group by product_id
-      );
+  from (select *
+             , rank() over(partition by product_id order by year asc) as rn
+          from Sales
+) as subq
+  where rn = 1
