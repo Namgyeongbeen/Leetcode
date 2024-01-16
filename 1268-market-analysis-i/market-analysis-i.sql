f34be1ec5b4733a0f26dfff5a0e4c9201f24402e
Gyeongbeen <152850843+Namgyeongbeen@users.noreply.github.com>
2024-01-16 11:43:30 +0900

    Time: 1932 ms (92.14%) | Memory: 0B (100.00%) - LeetSync

--- 1268-market-analysis-i/market-analysis-i.sql
+++ 1268-market-analysis-i/market-analysis-i.sql
@@ -3,5 +3,5 @@ SELECT u.user_id AS buyer_id,
        u.join_date,
        SUM(CASE WHEN YEAR(o.order_date) = 2019 THEN 1 ELSE 0 END) AS orders_in_2019
 FROM Users u
-LEFT JOIN Orders o ON u.user_id = o.buyer_id AND YEAR(o.order_date) = 2019
+LEFT JOIN Orders o ON u.user_id = o.buyer_id -- AND YEAR(o.order_date) = 2019
 GROUP BY u.user_id, u.join_date;
